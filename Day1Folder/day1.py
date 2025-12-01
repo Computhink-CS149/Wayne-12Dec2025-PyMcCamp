44c990e1cc7857594ebcb02dd2023b28ae383f03
Computhink-CS149 <cs149@computhink.com.sg>
2025-12-01 08:49:44 +0000

    Dec 1, 2025, 8:49 AM

--- Day1Folder/day1.py
+++ Day1Folder/day1.py
@@ -4,38 +4,60 @@ print("hello from day1")
 
 ########################################################################
 # Task 1:
-
+print("hello")
 
 
 ########################################################################
 # Task 2:
-
-
-
+print("I am Wayne Loo")
+print("I am 11 years old")
+print("I attend Kong Hwa School")
+print("If I had $2000 as a reward, I would buy a nintendo game")
 ########################################################################
 # Task 3:
-
-
-
+animal1="tiger"
+animal2="lion"
+animal3="leopard" 
+print(animal1)
+print(animal2)
+print(animal3)
 ########################################################################
 # Task 4:
-
-
-
+#num1=3
+#num2=7
+#num3=8
+#print(num1+num2+num3)
+#num4=num1*num2*num3
+#print(num4)
+#print(num4-num1+num2+num3)
 ########################################################################
 # Task 5:
-
-
-
+name="wayne"
+food="chicken rice"
+print("my name is " + name + " and I like to eat " + food)
 ########################################################################
 # Task 6:
+def fun1():
+    print("my name is wayne")
+    print("I am 11 years old")
+    print("I attend Kong Hwa School")
+    print("If I had $2000")
 
 
 
 ########################################################################
 # Task 7:
+def addition (num1,num2,num3):
+    print(num1+num2+num3)
 
+addition(1,2,3)
+addition(67,41,6741)
 
+def dvs (num1,num2):
+    print(num1/num2)
+
+dvs(8,2)
+########################################################################
 
 ########################################################################
 # Additional exercises:
--- Day1Folder/minecraft_day1.py
+++ Day1Folder/minecraft_day1.py
@@ -4,3 +4,45 @@
 
 
 ################## On Chat Commands Section #####################
+def teleport ():
+    agent.teleport_to_player()
+def move_forward (steps):
+    agent.move(FORWARD, steps)
+def move_back (steps):
+    agent.move(BACK,steps)
+def move_up (height):
+    agent.move(UP, height)
+def turn_right ():
+    agent.turn(TurnDirection.LEFT)
+def turn_left ():
+    agent.turn(LEFT)
+def move_down (height):
+    agent.move(DOWN, height)
+def obby1 ():
+   agent.move(FORWARD, 4) 
+   agent.move(LEFT, 4)
+   agent.move(FORWARD, 3)
+   agent.move(UP, 1)
+   agent.move(FORWARD, 1)
+   agent.move(UP, 1)
+   agent.move(FORWARD, 1)
+   agent.move(UP, 1)
+   agent.move(FORWARD, 1)
+   agent.move(FORWARD, 1)
+   agent.move(FORWARD, 1)
+   agent.move(DOWN, 1)
+   agent.move(FORWARD, 1)
+   agent.move(DOWN, 1)
+   agent.move(FORWARD, 1)
+   agent.move(DOWN, 1)
+
+
+
+player.on_chat("come", teleport)
+player.on_chat("mf", move_forward)
+player.on_chat("tr", turn_right)
+player.on_chat("mu", move_up)
+player.on_chat("mb", move_back)
+player.on_chat("tl", turn_left)
+player.on_chat("md", move_down)
+player.on_chat("obby",obby1)
