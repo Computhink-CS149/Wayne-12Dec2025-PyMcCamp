d41bc15405d5a702fcd6795de714b8ea2fe46e01
Computhink-CS149 <cs149@computhink.com.sg>
2025-12-02 16:59:47 +0800

    Update day2.py

--- Day2Folder/day2.py
+++ Day2Folder/day2.py
@@ -5,33 +5,61 @@ print("hello from day2")
 ########################################################################
 # Task 1:
 for count in range (10):
-  print("I am Wayne")
+    print("Hello I am Wayne")
 
 
 ########################################################################
 # Task 2:
-
-
+name = "Wayne"
+for letter in name :
+    print("Give me a " + letter )
+print("Who is the best??!?")
+print("WAYNE!!")
 
 ########################################################################
 # Task 3:
-
+for counter in range (10):
+    print(counter)
 
 
 ########################################################################
 # Task 4:
+for count in range(1,11):
+    print ( count)
 
 
 
 ########################################################################
 # Task 5:
+for count in range (5,33):
+    print(count)
 
-
-
+print("next line")
 ########################################################################
 # Task 6:
+for count in range (50, 0, -1):
+    print(count)
 
-
-
+print("next line")
 ########################################################################
 # Additional exercises:
+for count in range(67):
+    print(count)
+print("next line")
+for count in range(11):
+    print (count)
+print("next line")
+for count in range(7,33):
+    print(count)
+print("next line")
+for count in range (65, 100):
+    print(count)
+print("next line")
+for count in range(2,33,2):
+    print(count)
+print("next line")
+for count in range(5,101,5):
+    print (count)
+print("next line")
+for count in range (100,-1,-2):
+    print (count)
