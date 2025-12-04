0c827fc4b877990bdc3150f177febd99fbdad9ef
Computhink-CS149 <cs149@computhink.com.sg>
2025-12-04 06:47:06 +0000

    Dec 4, 2025, 6:47 AM

--- Day4Folder/day4.py
+++ Day4Folder/day4.py
@@ -1,14 +1,53 @@
 # Write all your codes for Day 4 here.
 # COMMENT out the previous task before going on to the next task
-print("hello from day4")
+# print("hello from day4")
 
 ########################################################################
 # Task 1:
+# counter = 0
+# while counter < 10:
+#     print(counter )
+#     counter += 1
+
+# counter = 5
+# while counter < 33:
+#     print(counter)
+#     counter += 1
+
+# counter = 50
+# while counter > 0:
+#     print (counter)
+#     counter -= 1
+
+
+
+
+
+
+
+
+
 
 
 
 ########################################################################
 # Task 2:
+
+riddle = "What is tall when it is young and short when it is old?"
+correct_ans = "candle"
+user_ans= input(riddle)
+
+while user_ans != correct_ans:
+    input(riddle)
+
+
+
+
+
+
+
+
+
 
 
 
